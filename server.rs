a04d9b600e5597b187e18b72e82528e5b76ff2d5
James Devlin <devlin.ja@husky.neu.edu>
2020-12-19 06:15:25 -0500

    Implemented rust server
    Created a `hello world` server implementation in rust.

---
+++ server.rs
@@ -0,0 +1,26 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 20]; // 20 byte buffer
+
+    let size = stream.read(&mut data).expect("Could not read from socket!");
+    stream.write(&data[0..size]).unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:43210").unwrap();
+    println!("Server listening...");
+
+    // accept connections and process them, each in a new spawned thread
+    for stream in listener.incoming().map(|stream| stream.unwrap()) {
+        println!("New connection: {}", stream.peer_addr().unwrap());
+        thread::spawn(move || {
+            // connection succeeded
+            handle_client(stream)
+        });
+    }
+    // close the socket server
+    drop(listener);
+}
