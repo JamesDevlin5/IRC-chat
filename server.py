1d079df84c1fa531b19b17f54d9da4b59fdb5a0f
James Devlin <devlin.ja@husky.neu.edu>
2020-12-19 03:13:21 -0500

    Created Server object
    Created the server object, which handles sending and receiving messages
    over the network. Default implementation is provided for now. Once a
    connection is accepted, its corresponding channel and address are
    returned to be used by the caller.

--- server.py
+++ server.py
@@ -1,17 +1,38 @@
 import socket
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    sock.listen()
-    conn, addr = sock.accept()
-    with conn:
-        print(f'Connected to {addr}')
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f'Sending {repr(data)}')
-            conn.sendall(data)
+
+class Server:
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def bind(self, addr):
+        self._socket.bind(addr)
+
+    def listen(self):
+        self._socket.listen()
+
+    def accept(self):
+        return self._socket.accept()
+
+    def close(self):
+        self._socket.shutdown(socket.SHUT_RDWR)
+        self._socket.close()
+
+
+server = Server()
+server.bind((HOST, PORT))
+server.listen()
+
+
+conn, addr = server.accept()
+with conn:
+    print(f"Connected to {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(f"Sending {repr(data)}")
+        conn.sendall(data)
