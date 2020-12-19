f353d707e3304d0bf3f291bd5be11e0fd39f7aa6
James Devlin <devlin.ja@husky.neu.edu>
2020-12-19 03:04:55 -0500

    Created Client object
    Created the client object, which handles sending and receiving messages
    over the network. Default implementation is provided for now.

--- client.py
+++ client.py
@@ -1,11 +1,30 @@
 import socket
 
-HOST = '127.0.0.1'
+HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((HOST, PORT))
-    sock.sendall(b'Hello World!')
-    data = sock.recv(1024)
 
-print(f'Received {repr(data)}')
+class Client:
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, addr):
+        self._socket.connect(addr)
+
+    def send(self, msg):
+        self._socket.sendall(msg)
+
+    def recv(self):
+        return self._socket.recv(1024)
+
+    def close(self):
+        self._socket.shutdown(socket.SHUT_RDWR)
+        self._socket.close()
+
+
+client = Client()
+client.connect((HOST, PORT))
+client.send(b"Hello World!")
+data = client.recv()
+
+print(f"Received {repr(data)}")
