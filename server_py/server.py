af45f26d89e03d7cadcdf91690bba7afd69b8776
James Devlin <devlin.ja@husky.neu.edu>
2020-12-20 13:08:17 -0500

    Encapsulated parser and connection objects
    Encapsulated the parser in a function definition, allowing for easy
    argument parsing. Also created an object representing the connection to
    a client, which is capable of sending and receiving data.

--- server_py/server.py
+++ server_py/server.py
@@ -1,12 +1,34 @@
+import argparse
 import socket
+import ipaddress
 
-HOST = "127.0.0.1"
-PORT = 43210
+# Default Values
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 43210
+
+# Creates the parser with the arguments: <host> <port>
+def create_parser():
+    parser = argparse.ArgumentsParser(description="An IRC server.")
+    parser.add_argument(
+        "host",
+        type=ipaddress.ip_address,
+        default=DEFAULT_HOST,
+        help="the host to listen on",
+        required=False,
+    )
+    parser.add_argument(
+        "port", type=int, default=DEFAULT_PORT, help="the target port to listen on"
+    )
+    return parser
 
 
 class Server:
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def startup(self, addr):
+        self.bind(addr)
+        self.listen()
 
     def bind(self, addr):
         self._socket.bind(addr)
@@ -21,20 +43,35 @@ class Server:
         self._socket.close()
 
 
+args = create_parser().parse_args()
+target = (args.host.exploded, args.port)
 server = Server()
-server.bind((HOST, PORT))
-server.listen()
+server.startup(target)
+
+
+class Connection:
+    def __init__(self, sock):
+        self._socket = sock
+
+    def send(self, msg):
+        self._socket.sendall(msg.encode("utf-8"))
+
+    def recv(self):
+        return self._socket.recv(1024)
+
+    def close(self):
+        self._socket.close()
 
 
 conn, addr = server.accept()
+client = Connection(conn)
 
 print(f"Connected to {addr}")
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    print(f"Sending {repr(data)}")
-    conn.sendall(data)
 
-conn.close()
+data = client.recv()
+print(f"Received: {repr(data)}")
+
+conn.sendall(data)
+
+client.close()
 server.close()
